3961fd1aa8b682679a6f36dac426c5732eaf7b4a
dudinio86 <dudinio86@gmail.com>
2025-06-07 14:54:20 +0300

    07062025

--- HomeWork/API DOM/script.js
+++ HomeWork/API DOM/script.js
@@ -1,59 +1,33 @@
-// import fs from "node:fs/promises";
-
-// async function readJsonFile(filePath) {
-//   try {
-//     const data = await fs.readFile(filePath, "utf8");
-//     const jsonData = JSON.parse(data);
-//     return jsonData;
-//   } catch (error) {
-//     console.error("Ошибка при чтении файла:", error);
-//     throw error;
-//   }
-// }
-
-// async function main(){
-//   const jsonData = await readJsonFile("./data.json");
-//   console.log(jsonData);
-
-// const blockEl = document.querySelector(".content");
-
-// jsonData.foreach((el) => {
-//   const nameEl = document.createElement("p");
-//   nameEl.classList("text");
-//   nameEl.textContent = `Наименование: ${el.name}`;
-//   blockEl.appendChild(nameEl);
-
-//   const timeEl = document.createElement("p");
-//   timeEl.classList("text");
-//   timeEl.textContent = `Наименование: ${el.time}`;
-//   blockEl.appendChild(timeEl);
-//   console.log(el.time);
-// });
-// }
-
-// main();
-
-
-const loadDataButton = document.getElementById("loadData");
-const outputDiv = document.getElementById("output");
-
-loadDataButton.addEventListener("click", async () => {
+async function main() {
   try {
-    const corsHeaders = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-      "Access-Control-Max-Age": "86400",
-    };
-    const response = await fetch("/api/data"); // Запрос к серверу
-    const jsonData = await response.json();
+    const blockEl = document.querySelector(".content");
+    const response = await fetch("/api/data"); 
+    let jsonData = await response.json();
 
-    outputDiv.textContent = JSON.stringify(jsonData, null, 2);
+    jsonData.map((el) => {
+      const nameEl = document.createElement("p");
+      nameEl.classList.add("text");
+      nameEl.textContent = `Наименование предмета: ${el.name}`;
+      blockEl.appendChild(nameEl);
 
-    // const response = await fetch("/1"); // Запрос к серверу
-    // const jsonData = await response.text();
-    // outputDiv.textContent = jsonData;
+      const timeEl = document.createElement("p");
+      timeEl.classList.add("text");
+      timeEl.textContent = `Время: ${el.time}`;
+      blockEl.appendChild(timeEl);
+      
+      const maxEl = document.createElement("p");
+      maxEl.classList.add("text");
+      maxEl.textContent = `Максимальное кол-во мест для записи: ${el.max}`;
+      blockEl.appendChild(maxEl);
+
+      const numEl = document.createElement("p");
+      numEl.classList.add("text");
+      numEl.textContent = `Кол-во записанных: ${el.numberRecorded}`;
+      blockEl.appendChild(numEl);
+    });
   } catch (error) {
-    outputDiv.textContent = "Ошибка при загрузке данных: " + error;
     console.error(error);
   }
-});
+}
+
+main();
--- HomeWork/API DOM/server.js
+++ HomeWork/API DOM/server.js
@@ -1,34 +1,43 @@
-const express = require('express');
-const fs = require('node:fs/promises');
-const cors = require('cors');  // Import the cors middleware
+const express =require("express");
+const fs =require("node:fs/promises");
+ const cors =require("cors"); // Import the cors middleware
+ const path = require('path');
 
 const app = express();
 const port = 3000;
 
-app.use(cors()); // Enable CORS for all routes  (VERY IMPORTANT)
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/index.html`);
-  });
+app.use(express.static(path.join(__dirname, ''), {
+  setHeaders: (res, path, stat) => {
+    if (path.endsWith('.js')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    }
+  }
+}));
 
-  app.get('/1', (req, res) => {
-    res.send(`hello`);
-  });
+app.get("/", (req, res) => {
+ 
+  res.sendFile(path.join(__dirname, 'index.html'));
+  
+  res.sendFile(path.join(__dirname,'script.js'));
+});
 
+app.get("/1", (req, res) => {
+  res.send(`hello`);
+});
 
-app.get('/api/data', async (req, res) => {
+app.get("/api/data", async (req, res) => {
   try {
-   
-    const data = await fs.readFile('./data.json', 'utf8');
+    const data = await fs.readFile('./data.json', "utf8");
     const jsonData = JSON.parse(data);
     res.json(jsonData);
   } catch (error) {
-    console.error('Ошибка на сервере:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Ошибка на сервере:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 app.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
 });
-
